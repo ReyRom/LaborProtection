a1dc46c4cff7d4d4dea4d962cb810db548e518b7
ReyRom <s03roman@mail.ru>
2022-11-23 21:22:49 +0300

    вв

---
+++ LaborProtection/LaborProtectionClient/Data/Creator.cs
@@ -0,0 +1,27 @@
+﻿using LaborProtectionClient.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborProtectionClient.Data
+{
+    internal static class Creator
+    {
+        public static IAnswerBox Create(this QuestionType type)
+        {
+            switch (type)
+            {
+                case QuestionType.One:
+                    return new RadioAnswerBox();
+                case QuestionType.Many:
+                    return new CheckAnswerBox();
+                case QuestionType.Text:
+                    return new TextAnswerBox();
+                default: 
+                    return null;
+            }
+        }
+    }
+}
---
+++ LaborProtection/LaborProtectionClient/Extensions/Creator.cs
@@ -0,0 +1,18 @@
+﻿using LaborProtectionClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LaborProtectionClient.Extensions
+{
+    internal static class Creator
+    {
+        public static Control Create(this QuestionType type)
+        {
+            
+        }
+    }
+}
